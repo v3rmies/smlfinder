bfb24fc3fea3285e2bd1be8cd67e96e020723122
v3rmies <105950299+v3rmies@users.noreply.github.com>
2025-03-31 23:01:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,15 @@ st.set_page_config(page_title="SML Finder ~ Zirzux", page_icon="https://i.imgur.
 # ✅ Hide Streamlit's menu, footer, and deploy button
 hide_streamlit_style = """
     <style>
-        #MainMenu {visibility: hidden;}
-        footer {visibility: hidden;}
-        header {visibility: hidden;}
-        .stDeployButton {display: none !important;}
+        #MainMenu {visibility: hidden;} /* Hide menu */
+        footer {visibility: hidden;} /* Hide footer */
+        header {visibility: hidden;} /* Hide header */
+        .stDeployButton {display: none !important;} /* Hide deploy button */
+        [data-testid="stToolbar"] {display: none !important;} /* Hide "Manage App" button */
     </style>
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 
 # ✅ Streamlit UI Title with Logo
 st.markdown("<h1 style='text-align: center;'><img src='https://i.imgur.com/pWQOKtC.png' width='40' height='40' style='vertical-align: middle;' /> Cuz why not?</h1>", unsafe_allow_html=True)
